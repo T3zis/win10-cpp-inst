0b077e8cccc0b86d4878284283b96cbcf825b590
Sergunys <quoqy@mail.ru>
2024-04-17 20:06:54 +0300

    1 update 2kr

--- 2kr/code/3_car.h
+++ 2kr/code/3_car.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 /*______HEADER-FILES______*/
-#include "2_vehicle .h"
+#include "2_vehicle.h"
 /*________________________*/
 
 
@@ -14,8 +14,12 @@ protected:
 public:
 	____car() : ____vehicle(), num_wheels{ 0 }, num_windows{ 0 } {}
 	____car(double _wght, double _engPwr, double _spd, int _nWhl, int _nWinds)
-		: ____vehicle(_wght, _engPwr, _spd), num_wheels{ _nWhl }, num_windows{ _nWinds } {}
+		        : ____vehicle(_wght, _engPwr, _spd), num_wheels{ _nWhl }, num_windows{ _nWinds } {}
 
+	int get_num_wheels()  const { return num_wheels; }
+	int get_num_windows() const { return num_windows; }
 
+	void set_num_wheels(int new_num_wheels)   { num_wheels = new_num_wheels; }
+	void set_num_windows(int new_num_windows) { num_windows = new_num_windows; }
 };
 /*___________________________________*/
---
+++ 2kr/code/4_etc_car.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+/*______HEADER-FILES______*/
+#include "3_car.h"
+/*________________________*/
+
+/*_____________class-sport-car_____________*/
+
+class ____sport_car : public ____car {
+protected:
+	int time_up_to_100km; // время разгона до 100
+	int num_turbines;     // кол-во турбин
+
+public:
+	____sport_car() : ____car(), time_up_to_100km{ 0 }, num_turbines{ 0 } {}
+	____sport_car(double _wght, double _engPwr, double _spd, int _nWhl, int _nWinds, int _tUT100, int _nTrb)
+		: ____car(_wght, _engPwr, _spd, _nWhl, _nWinds), time_up_to_100km{ _tUT100 }, num_turbines{ _nTrb } {}
+
+	~____sport_car() override {}
+
+
+};
+/*_________________________________________*/
+
+
+
+class ____truck : public ____car {
+protected:
+	int mileage;     // пробег
+	int body_volume; // объем кузова
+
+public:
+	____truck() : ____car(), mileage{ 0 }, body_volume{ 0 } {}
+	____truck(double _wght, double _engPwr, double _spd, int _nWhl, int _nWinds, int _mil, int _bVol)
+		: ____car(_wght, _engPwr, _spd, _nWhl, _nWinds), mileage{ _mil }, body_volume{ _bVol } {}
+
+	~____truck() override {}
+
+
+
+};
